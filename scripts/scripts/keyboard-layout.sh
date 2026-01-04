53d01d43c37964cb7bf3d8d819a23b31ef43feb1
uylong <songuylong24@gmail.com>
2026-01-05 04:51:12 +0700

    change

--- scripts/scripts/keyboard-layout.sh
+++ scripts/scripts/keyboard-layout.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Get current keyboard layout and display short code
+# Get current keyboard layout from the main keyboard device and display short code
 
-layout=$(hyprctl devices -j | jq -r '.keyboards[0].active_keymap')
+layout=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
 
 case "$layout" in
     *"English"*|*"US"*)
         echo "US"
         ;;
-    *"Khmer"*|*"Cambodia"*)
+    *"Khmer"*|*"Cambodia"*|*"Khmer (Cambodia)"*)
         echo "KH"
         ;;
     *)
